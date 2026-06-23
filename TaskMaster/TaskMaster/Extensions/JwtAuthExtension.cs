7ea4c57af1657b5d6b288fa1c66dbbc743841522
Justyne-bootcamp <102782791+Justyne-bootcamp@users.noreply.github.com>
2026-06-23 21:34:18 +0800

    Clean up on Program.cs. Create extension class to handle JwtAuth

---
+++ TaskMaster/TaskMaster/Extensions/JwtAuthExtension.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace TaskMaster.Extensions;
+public static class JwtAuthExtension
+{
+    public static IServiceCollection AddJwtAuth(this IServiceCollection services, IConfiguration config)
+    {
+        var jwtSettings = config.GetSection("JwtConfiguration");
+
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ClockSkew = TimeSpan.Zero,
+
+                ValidIssuer = jwtSettings["Issuer"],
+                ValidAudience = jwtSettings["Audience"],
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["Key"]))
+            };
+        });
+        return services;
+    }
+}
--- TaskMaster/TaskMaster/Program.cs
+++ TaskMaster/TaskMaster/Program.cs
@@ -11,6 +11,7 @@ using TaskMaster.Models;
 using TaskMaster.Models.Options;
 using TaskMaster.Repositories;
 using TaskMaster.Services;
+using TaskMaster.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -37,25 +38,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 });
 builder.Services.Configure<GoogleAuthOptions>(builder.Configuration.GetSection("GoogleAuthConfiguration"));
 
-var jwtSettings = builder.Configuration.GetSection("JwtConfiguration");
-
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-
-            ClockSkew = TimeSpan.Zero,
-
-            ValidIssuer = jwtSettings["Issuer"],
-            ValidAudience = jwtSettings["Audience"],
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["Key"]))
-        };
-    });
+builder.Services.AddJwtAuth(builder.Configuration);
 
 builder.Services.AddAuthorization();
 
@@ -71,6 +54,7 @@ builder.Services.AddCors(options =>
 });
 
 var app = builder.Build();
+
 app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseCors("AllowTaskMasterClient");
