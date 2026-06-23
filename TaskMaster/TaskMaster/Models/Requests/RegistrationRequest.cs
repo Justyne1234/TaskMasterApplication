b87ec186731e27e4e8ce14b4319f81b4a41fdb4d
Justyne-bootcamp <102782791+Justyne-bootcamp@users.noreply.github.com>
2026-06-23 20:00:58 +0800

    Prevent registration with no password on traditional way

--- TaskMaster/TaskMaster/Models/Requests/RegistrationRequest.cs
+++ TaskMaster/TaskMaster/Models/Requests/RegistrationRequest.cs
@@ -1,7 +1,11 @@
-﻿namespace TaskMaster.Models.Requests;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskMaster.Models.Requests;
 public class RegistrationRequest
 {
+    [Required]
     public string Username { get; set; }
+    [Required]
     public string Password { get; set; }
     public string GoogleId { get; set; }
     public string AuthenticationMethod { get; set; }
